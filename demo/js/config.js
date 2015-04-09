f94ed846e93015fe28847b9b5744ccc78f9f0dcb
Chris <cdiana.media@gmail.com>
2015-04-09 15:46:55 -0400

    updated homepage

---
+++ demo/js/config.js
@@ -0,0 +1,69 @@
+$(function() {
+	simpleCart({
+
+	    // array representing the format and columns of the cart, see
+	    // the cart columns documentation
+	    cartColumns: [
+	        { attr: "name" , label: "Name" },
+	        { attr: "price" , label: "Price", view: 'currency' },
+	        { view: "decrement" , label: false },
+	        { attr: "quantity" , label: "Qty" },
+	        { view: "increment" , label: false },
+	        { attr: "total" , label: "SubTotal", view: 'currency' },
+	        { view: "remove" , text: "Remove" , label: false }
+	    ],
+
+	    // "div" or "table" - builds the cart as a table or collection of divs
+	    cartStyle: "div",
+
+	    // how simpleCart should checkout, see the checkout reference for more info
+	    checkout: {
+	        type: "PayPal" ,
+	        email: "you@yours.com"
+	    },
+
+	    // set the currency, see the currency reference for more info
+	    currency: "USD",
+
+	    // collection of arbitrary data you may want to store with the cart,
+	    // such as customer info
+	    data: {},
+
+	    // set the cart langauge (may be used for checkout)
+	    language: "english-us",
+
+	    // array of item fields that will not be sent to checkout
+	    excludeFromCheckout: [],
+
+	    // custom function to add shipping cost
+	    shippingCustom: null,
+
+	    // flat rate shipping option
+	    shippingFlatRate: 0,
+
+	    // added shipping based on this value multiplied by the cart quantity
+	    shippingQuantityRate: 0,
+
+	    // added shipping based on this value multiplied by the cart subtotal
+	    shippingTotalRate: 0,
+
+	    // tax rate applied to cart subtotal
+	    taxRate: 0,
+
+	    // true if tax should be applied to shipping
+	    taxShipping: false,
+
+	    // event callbacks
+	    beforeAdd               	: null,
+	    afterAdd                	: null,
+	    load                    	: null,
+	    beforeSave              	: null,
+	    afterSave               	: null,
+	    update                  	: null,
+	    ready                   	: null,
+	    checkoutSuccess             : null,
+	    checkoutFail                : null,
+	    beforeCheckout              : null
+
+	});
+});
