3006570381184acde7133f0ced1a2cf3e9189156
Chris <cdiana.media@gmail.com>
2015-12-29 23:20:53 -0500

    add google sheet demo

---
+++ demo/google-sheets-demo/js/config.js
@@ -0,0 +1,86 @@
+$(function() {
+	simpleCart({
+
+	    // array representing the format and columns of the cart, see
+	    // the cart columns documentation
+	    cartColumns: [
+	        { attr: "name" , label: "Name" },
+	        { attr: "price" , label: "Price", view: 'currency' },
+	        { view: "decrement" , label: false },
+	        { attr: "quantity" , label: "Qty" },
+	        { view: "increment" , label: false },
+	        { attr: "total" , label: "SubTotal", view: 'currency' },
+	        { view: "remove" , text: "Remove" , label: false }
+	    ],
+
+	    // "div" or "table" - builds the cart as a table or collection of divs
+	    cartStyle: "div",
+
+	    // how simpleCart should checkout, see the checkout reference for more info
+	    checkout: {
+	        type: "PayPal" ,
+	        email: "you@yours.com"
+	    },
+
+	    // set the currency, see the currency reference for more info
+	    currency: "USD",
+
+	    // collection of arbitrary data you may want to store with the cart,
+	    // such as customer info
+	    data: {},
+
+	    // set the cart langauge (may be used for checkout)
+	    language: "english-us",
+
+	    // array of item fields that will not be sent to checkout
+	    excludeFromCheckout: [
+	    	'qty',
+	    	'thumb'
+	    ],
+
+	    // custom function to add shipping cost
+	    shippingCustom: null,
+
+	    // flat rate shipping option
+	    shippingFlatRate: 0,
+
+	    // added shipping based on this value multiplied by the cart quantity
+	    shippingQuantityRate: 0,
+
+	    // added shipping based on this value multiplied by the cart subtotal
+	    shippingTotalRate: 0,
+
+	    // tax rate applied to cart subtotal
+	    taxRate: 0,
+
+	    // true if tax should be applied to shipping
+	    taxShipping: false,
+
+	    // event callbacks
+	    beforeAdd               	: null,
+	    afterAdd                	: null,
+	    load                    	: null,
+	    beforeSave              	: null,
+	    afterSave               	: null,
+	    update                  	: null,
+	    ready                   	: null,
+	    checkoutSuccess             : null,
+	    checkoutFail                : null,
+	    beforeCheckout              : null
+
+	});
+
+	simpleStore.init({
+
+		// brand can be text or image URL
+		brand : "SimpleStore Google Sheets Demo",
+
+		// numder of products per row (accepts 1, 2 or 3)
+		numColumns : 3,
+
+		// Google Spreadsheet ID
+		spreadsheetID : "1eDIC1zfBP3OWChohDDzIv49GBcxxbX8IkYPKBYPZk58"
+
+	});
+
+});
---
+++ demo/google-sheets-demo/js/google-sheets.js
@@ -0,0 +1,169 @@
+/*
+ * SimpleStore Google Sheets Plugin
+ * To use Google spreadsheet as your database, follow the steps below:
+ * 1. Use the "DemoSpreadsheet.xlsx" as a starting point
+ * 2. Create a new Google spreadsheet
+ * 3. Set sharing permissions to either “Public” or set to “Anyone with link can view”
+ * 4. Publish the sheet (File -> Publish to the web -> Publish)
+ * 5. Add the spreadsheet ID to your 'config.js' ( spreadsheetID : "XXXXXXXXXXXXXXXXXXXXXXX" )
+ */
+
+simpleStore.plugins.google = (function() {
+
+	var storeProducts = verifyProducts = [];
+
+	function getSpreadsheetData(s, verify, callback) {
+
+		verify = typeof verify !== 'undefined' ? verify : false;
+
+		var hostname = "https://spreadsheets.google.com";
+		var format = "json";
+		var spreadsheetURL = hostname + "/feeds/worksheets/" + s.spreadsheetID + "/public/full?alt=" + format;
+		var mainsheetURL = hostname + "/feeds/list/" + s.spreadsheetID + "/od6/public/values?alt=" + format;
+		var settingsSheetName = "Settings";
+		var productsSheetName = "Products";
+		var sheetIDs = {};
+
+		function getSheetInfo (url, callback) {
+			// Need to do this because od6 is default Google Sheet ID
+			$.getJSON(url)
+				.done(function(data) {
+
+					var sheets = data.feed.entry;
+
+					$(sheets).each(function(i, sheet) {
+
+						var title = sheet.title.$t;
+						var id = sheet.id.$t;
+						var sheetID = id.substr(id.lastIndexOf('/') + 1);
+
+						if(title == settingsSheetName) {
+							sheetIDs.settingsSheetID = sheetID;
+						}
+						if(title == productsSheetName) {
+							sheetIDs.productsSheetID  = sheetID;
+						}
+					});
+					callback(sheetIDs.settingsSheetID);
+					loadProductData(sheetIDs.productsSheetID);
+				});
+		}
+
+		function loadSiteSettings (id, callback) {
+
+			var settingsSheetURL = hostname + "/feeds/list/" + s.spreadsheetID + "/" + id + "/public/values?alt=" + format;
+
+			$.getJSON(settingsSheetURL)
+				.done(function(data) {
+					var data = data.feed.entry;
+					var s = simpleStore.settings;
+
+					if(data[0]) {
+
+						var siteName = data[0].gsx$sitenametextorimagelink.$t;
+						var columns = data[0].gsx$columns123.$t;
+
+						if (siteName) {
+							s.brand = siteName;
+						}
+						if (columns) {
+							s.numColumns = columns;
+						}
+
+						simpleStore.setLayout(s);
+					}
+				});
+		}
+
+		function loadProductData (id) {
+
+			var productsSheetURL = hostname + "/feeds/list/" + s.spreadsheetID + "/" + id + "/public/values?alt=" + format;
+
+			// Get Main Sheet Products data
+			$.getJSON(productsSheetURL)
+				.done(function(data) {
+
+					var productsData = data.feed.entry;
+
+					// Build products
+					$(productsData).each(function(i) {
+
+						var options = this.gsx$options.$t;
+						var setOptions = function(options) {
+							var productOptions = [];
+							if(options) {
+								var opts = options.split(";").filter(function(el) {return el.length != 0});
+								$(opts).each(function(i, option) {
+									var opt = option.trim().split(":"),
+										key = opt[0],
+										val = opt[1],
+										optObj = {};
+
+									optObj[key] = val;
+									productOptions.push(optObj);
+								});
+							}
+							return productOptions;
+						};
+
+						// Get product values
+						var product = {
+							name : this.gsx$name.$t,
+							price : this.gsx$price.$t,
+							description : this.gsx$description.$t,
+							options : setOptions(options),
+							image : this.gsx$image.$t
+						};
+
+						if (verify) {
+							verifyProducts.push(product);
+						} else {
+							storeProducts.push(product);
+						}
+					});
+					callback();
+				})
+				.fail(function(data){
+					if (verify) {
+						var errorMsg = 'There was an error validating your cart.';
+					} else {
+						var errorMsg = 'Error loading spreadsheet data. Make sure the spreadsheet ID is correct.';
+					}
+					setTimeout(function(){ simpleStore.renderError(s, errorMsg); }, 1000);
+				});
+		}
+
+		// Get Sheet data
+		getSheetInfo(spreadsheetURL, loadSiteSettings);
+	}
+
+	function validatePrices(s, checkoutData) {
+		verifyProducts = [];
+
+		getSpreadsheetData(s, true, function() {
+			if(simpleStore.verifyCheckoutData(checkoutData, verifyProducts, true)) {
+        		simpleStore.checkout();
+			} else {
+				var errorMsg = 'There was an error validating your cart.';
+				simpleStore.renderError(s, errorMsg);
+			}
+		});
+	}
+
+	return {
+		init: function(callback) {
+			var s = simpleStore.settings;
+
+			// Clears out brand to allow for spreadsheet site name
+			s.brand = "";
+			simpleStore.setLayout(s);
+
+			getSpreadsheetData(s, false, function(){
+				callback(storeProducts);
+			});
+		},
+		validate: function(checkoutData) {
+			validatePrices(simpleStore.settings, checkoutData);
+		}
+	};
+})();
